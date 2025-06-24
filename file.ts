cd8e273b0872e706d22b989a9c72ce65b3464071
Harveer Deol <harveerdeol53@gmail.com>
2025-06-24 16:02:09 -0600

    calculator built

---
+++ file.ts
@@ -0,0 +1,14 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+
+const calculator = (a: number, b:number, op:Operation) =>{
+    if (op === 'multiply'){
+        return(a*b)
+    } else if (op === 'add'){
+        return(a+b)
+    } else if (op === 'divide'){
+        if(b === 0){
+            return `can't divide by zero`}
+        return a/b
+    }
+}
