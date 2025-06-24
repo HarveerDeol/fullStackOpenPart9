749987d7a2ef39cebf71358c8049ce2836c42765
Harveer Deol <harveerdeol53@gmail.com>
2025-06-24 16:22:56 -0600

    new file imports

--- file.ts
+++ file.ts
@@ -3,12 +3,12 @@ type Operation = 'multiply' | 'add' | 'divide';
 
 const calculator = (a: number, b:number, op:Operation) =>{
     if (op === 'multiply'){
-        return(a*b)
+        return a * b
     } else if (op === 'add'){
-        return(a+b)
-    } else if (op === 'divide'){
+        return a + b
+        } else if (op === 'divide'){
         if(b === 0){
             return `can't divide by zero`}
-        return a/b
+        return a / b
     }
 }
