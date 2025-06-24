81718c9992c828698e34a0359e79e294f4ed5c2d
Harveer Deol <harveerdeol53@gmail.com>
2025-06-24 16:41:47 -0600

    used switch cases to allow for error  handling

--- file.ts
+++ file.ts
@@ -1,14 +1,18 @@
 type Operation = 'multiply' | 'add' | 'divide';
 
 
-const calculator = (a: number, b:number, op:Operation) =>{
-    if (op === 'multiply'){
-        return a * b
-    } else if (op === 'add'){
-        return a + b
-        } else if (op === 'divide'){
-        if(b === 0){
-            return `can't divide by zero`}
-        return a / b
-    }
+const calculator = (a: number, b:number, op:Operation) : number =>{
+    switch(op) {
+        case 'multiply':
+            return a * b
+        case 'add':
+            return a + b
+        case 'divide':
+            if (b === 0) throw new Error('cannot divide by zero')
+            return a / b
+        default:
+            throw new Error ('please enter add, mulitply, or divide')
+ }
 }
+
+calculator(1, 2,'add')
